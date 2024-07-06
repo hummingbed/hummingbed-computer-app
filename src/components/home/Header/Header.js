6626adf5b4cf3c36255fdb6dce4970b289ea6090
MichaelMenebraye <michael@dufuna.org>
2024-07-06 20:50:48 +0100

    update

--- src/components/home/Header/Header.js
+++ src/components/home/Header/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
           <Link to="/">
             <div>
               
-              Hummingbed-collections
+            Hummingbed-gadget
             </div>
           </Link>
           <div>
